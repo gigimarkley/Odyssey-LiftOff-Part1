b7e3f3276d5c2e0afc48d73f944a1b302f568123
Gigi Markley <gigimarkley@Admins-MacBook-Pro.local>
2021-10-28 15:15:03 -0700

    Apollo Client setup

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,31 +1,32 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-"how to tell the GraphQL server what to retrieve when we query it. Remember, we don't have multiple specific endpoints to target different types like a REST API does. Instead, we define a special Query type."
-"The fields of this type are entry points into the rest of our schema. These are the top-level fields that our client can query for."
-type Query {
-    "Get tracks array for homepage grid"
+  type Query {
+    "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-}  
+  }
 
-"A track is a group of Modules that teaches about a specific topic"
+  "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
-    "track's title"
+    "The track's title"
     title: String!
-    "track's author"
+    "The track's main Author"
     author: Author!
-    "track's thumbnail to display on page details"
+    "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "track's approx length in min"
+    "The track's approximate length to complete, in minutes"
     length: Int
-    "number of modules the track contains"
+    "The number of modules this track contains"
     modulesCount: Int
   }
+
   "Author of a complete Track or a Module"
   type Author {
     id: ID!
+    "Author's first and last name"
     name: String!
+    "Author's profile picture"
     photo: String
   }
 `;
